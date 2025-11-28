fix: fix a wrong index for `relative action from observation[-1]` with dual arm config

diffusion_policy/common/action_utils.py
@@ -65,10 +65,17 @@ def interpolate_actions_with_ratio(actions: np.ndarray, N: int):
 def absolute_actions_to_relative_actions(
     actions: np.ndarray,
     base_absolute_action=None,
-    # 'relative', 'only-y-train', 'only-y-inference'
     action_representation: str = 'relative'
 ):
+    """
+    actions: (T, D)
+    base_absolute_action: (D,)
+    action_representation: 'relative', 'only-y-train', 'only-y-inference'
+        If you train the model with `only-y-train`, 
+        you will need `only-y-inference` during real-machine inference.
+    """
     assert action_representation in ['relative', 'only-y-train', 'only-y-inference']
+    assert base_absolute_action.shape[0] == actions.shape[-1]
     actions = actions.copy()
     T, D = actions.shape
 
@@ -125,10 +132,13 @@ def absolute_actions_to_relative_actions(
 def relative_actions_to_absolute_actions(
     actions: np.ndarray,
     base_absolute_action: np.ndarray,
-    # 'relative', 'only-y-train', 'only-y-inference'
     action_representation: str = 'relative'
 ):
+    """
+    See: absolute_actions_to_relative_actions
+    """
     assert action_representation in ['relative', 'only-y-train', 'only-y-inference']
+    assert base_absolute_action.shape[0] == actions.shape[-1]
     actions = actions.copy()
     T, D = actions.shape
 

diffusion_policy/dataset/real_pick_and_place_image_dataset.py
@@ -220,9 +220,13 @@ class RealPickAndPlaceImageDataset(BaseImageDataset):
         if self.n_latency_steps > 0:
             action = action[self.n_latency_steps:]
         if self.relative_action:
+            # Make the length 10 or 20, not 9 or 18.
+            # Shape in obs_dict[xxx] is (T, D).
             base_absolute_action = np.concatenate([
                 obs_dict['left_robot_tcp_pose'][-1] if 'left_robot_tcp_pose' in obs_dict else np.array([]),
-                obs_dict['right_robot_tcp_pose'][-1] if 'right_robot_tcp_pose' in obs_dict else np.array([])
+                obs_dict['right_robot_tcp_pose'][-1] if 'right_robot_tcp_pose' in obs_dict else np.array([]),
+                obs_dict['left_robot_gripper_width'][-1] if 'left_robot_gripper_width' in obs_dict else np.array([]),
+                obs_dict['right_robot_gripper_width'][-1] if 'right_robot_gripper_width' in obs_dict else np.array([]),
             ], axis=-1)
             action = absolute_actions_to_relative_actions(
                 action, base_absolute_action=base_absolute_action,
@@ -232,9 +236,10 @@ class RealPickAndPlaceImageDataset(BaseImageDataset):
             if self.relative_tcp_obs_for_relative_action:
                 for key in self.lowdim_keys:
                     if 'robot_tcp_pose' in key and 'wrt' not in key:
-                        obs_dict[key]  = absolute_actions_to_relative_actions(
+                        # calculate relative obs from the last obs (as image history)
+                        obs_dict[key] = absolute_actions_to_relative_actions(
                             obs_dict[key],
-                            base_absolute_action=base_absolute_action,
+                            base_absolute_action=obs_dict[key][-1],
                             action_representation=self.action_representation
                         )
 