e2126b3039cdb44fa3f6ec6bc972621bcbe1e046
Wendi Chen <chenwendi-andy@sjtu.edu.cn>
2026-01-08 22:41:04 +0800

    feat: update online dagger script

--- train_online_dagger.sh
+++ train_online_dagger.sh
@@ -3,6 +3,7 @@ OFFLINE_DATASET_PATH=/home/wendi/Desktop/dataset/blocksv2_100/replay_buffer.zarr
 DAGGER_IDENTIFIER=dagger_debug2
 INITIAL_CHECKPOINT_PATH=/home/wendi/Desktop/dp_ckpt/2025.12.31/11.34.10_train_diffusion_unet_timm_single_frame_blocksv2_100_rel/checkpoints/latest.ckpt
 RESUME=False
+INFERENCE_SERVER_URL=http://192.168.2.242:8080
 
 export HYDRA_FULL_ERROR=1 && \
 	python train.py \
@@ -13,5 +14,6 @@ export HYDRA_FULL_ERROR=1 && \
 	task.dataset.offline_dataset_path=${OFFLINE_DATASET_PATH} \
 	online_training.identifier=${DAGGER_IDENTIFIER} \
     online_training.initial_checkpoint_path=${INITIAL_CHECKPOINT_PATH} \
+	online_training.inference_server_url=${INFERENCE_SERVER_URL} \
 	training.resume=${RESUME} \
 	logging.mode=online
