d73f59711d6977f1f3ff26a88c0a1c2d451f024d
Wendi Chen <chenwendi-andy@sjtu.edu.cn>
2026-01-17 14:54:37 +0800

    chore: update configs in train_online_dagger

--- train_online_dagger.sh
+++ train_online_dagger.sh
@@ -1,9 +1,22 @@
-TASK_NAME=single_frame_blocksv2_100_rel
-OFFLINE_DATASET_PATH=/home/wendi/Desktop/dataset/blocksv2_100/replay_buffer.zarr
-DAGGER_IDENTIFIER=dagger_debug2
-INITIAL_CHECKPOINT_PATH=/home/wendi/Desktop/dp_ckpt/2025.12.31/11.34.10_train_diffusion_unet_timm_single_frame_blocksv2_100_rel/checkpoints/latest.ckpt
+INFERENCE_SERVER_URL=http://192.168.1.242:8080
+
+# TASK_NAME=single_frame_blocksv2_100_rel
+# OFFLINE_DATASET_PATH=/home/wendi/Desktop/dataset/blocksv2_100/replay_buffer.zarr
+# DAGGER_IDENTIFIER=blocksv2_dagger_0109
+# INITIAL_CHECKPOINT_PATH=/home/wendi/Desktop/dp_ckpt/2025.12.31/11.34.10_train_diffusion_unet_timm_single_frame_blocksv2_100_rel/checkpoints/latest.ckpt
+# RESUME=False
+
+# TASK_NAME=single_frame_pourmsg_100_rel
+# OFFLINE_DATASET_PATH=/home/wendi/Desktop/dataset/pourmsg_100/replay_buffer.zarr
+# DAGGER_IDENTIFIER=pourmsg_dagger_0110
+# INITIAL_CHECKPOINT_PATH=/home/wendi/Desktop/dp_ckpt/2025.12.31/01.10.33_train_diffusion_unet_timm_single_frame_pourmsg_100_rel/checkpoints/latest.ckpt
+# RESUME=False
+
+TASK_NAME=single_frame_towelv2_100_rel
+OFFLINE_DATASET_PATH=/home/wendi/Desktop/dataset/towelv2_100/replay_buffer.zarr
+DAGGER_IDENTIFIER=towelv2_dagger_online_0116
+INITIAL_CHECKPOINT_PATH=/home/wendi/Desktop/dp_ckpt/2026.01.15/19.35.00_train_diffusion_unet_timm_single_frame_towelv2_100_rel_dinov2/checkpoints/latest.ckpt
 RESUME=False
-INFERENCE_SERVER_URL=http://192.168.2.242:8080
 
 export HYDRA_FULL_ERROR=1 && \
 	python train.py \
