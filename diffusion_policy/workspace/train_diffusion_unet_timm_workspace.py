24e249979f24795586673070d057310c30d33620
Spphire <1216401387@qq.com>
2026-02-14 11:44:22 +0800

    fix multigpu log

--- diffusion_policy/workspace/train_diffusion_unet_timm_workspace.py
+++ diffusion_policy/workspace/train_diffusion_unet_timm_workspace.py
@@ -365,27 +365,25 @@ class TrainDiffusionUnetTimmWorkspace(BaseWorkspace):
 
                         all_preds, all_gt = accelerator.gather_for_metrics((pred_action, gt_action))
 
+                        # Prepare for masked prediction if left_eye_img exists
+                        has_head_img = 'left_eye_img' in obs_dict
+                        if has_head_img:
+                            obs_dict_masked = dict_apply(obs_dict, lambda x: x.clone() if isinstance(x, torch.Tensor) else x)
+                            obs_dict_masked['left_eye_img'] = torch.zeros_like(obs_dict_masked['left_eye_img'])
+                            result_masked = policy.predict_action(obs_dict_masked)
+                            pred_action_masked = result_masked['action_pred']
+                            all_preds_masked, _ = accelerator.gather_for_metrics((pred_action_masked, gt_action))
+                        else:
+                            all_preds_masked = None
+
                         if accelerator.is_main_process:
                             mse = torch.nn.functional.mse_loss(all_preds, all_gt)
                             step_log['train_action_mse_error'] = mse.item()
                             
-                            # Check if left_eye_img (head view) exists, and compute MSE with masked head image
-                            if 'left_eye_img' in obs_dict:
-                                # Create a masked version of obs_dict with left_eye_img set to 0 (black)
-                                obs_dict_masked = dict_apply(obs_dict, lambda x: x.clone() if isinstance(x, torch.Tensor) else x)
-                                obs_dict_masked['left_eye_img'] = torch.zeros_like(obs_dict_masked['left_eye_img'])
-                                
-                                # Predict action with masked observation
-                                result_masked = policy.predict_action(obs_dict_masked)
-                                pred_action_masked = result_masked['action_pred']
-                                
-                                all_preds_masked, _ = accelerator.gather_for_metrics((pred_action_masked, gt_action))
-                                
-                                # Log the MSE difference
+                            # Log head image masking results if available
+                            if has_head_img and all_preds_masked is not None:
                                 mse_no_head = torch.nn.functional.mse_loss(all_preds_masked, all_gt)
                                 step_log['train_action_mse_error_no_head'] = mse_no_head.item()
-                                
-                                # Log the difference for direct comparison
                                 step_log['train_action_mse_head_importance'] = (mse_no_head - mse).item()
 
                 accelerator.wait_for_everyone()
