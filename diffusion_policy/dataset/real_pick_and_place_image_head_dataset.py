17fb37c117123d9ce7fb57e321b4e36b058030ee
yibo <yibo@UIMicrodeMac-mini.local>
2026-02-16 21:26:15 +0800

    reset to prev augmentation

--- diffusion_policy/dataset/real_pick_and_place_image_head_dataset.py
+++ diffusion_policy/dataset/real_pick_and_place_image_head_dataset.py
@@ -221,11 +221,11 @@ class RealPickAndPlaceImageHeadDataset(BaseImageDataset):
                 img = apply_image_augmentation(img)
                 img = batch_resize_thwc(img, target_size=224, mode='pad')  # THWC uint8
             elif 'wrist' in key:
+                img = apply_image_augmentation(img)
                 img = batch_resize_thwc(img, target_size=224, mode='crop')  # THWC uint8
 
-                if np.random.rand() < 0.2:  # 20% 的概率
-                    #img = np.zeros_like(img)  # 20% 的概率全黑
-                    img = np.random.uniform(0, 0.01, size=img.shape).astype(np.float32)
+                # if np.random.rand() < 0.2:  # 20% 的概率
+                #     img = np.random.uniform(0, 0.01, size=img.shape).astype(np.float32)
             else:
                 logger.warning(f"Unknown image key: {key}, no resizing or augmentation applied to this key.")
                 raise NotImplementedError(f"Unknown image key: {key}")
